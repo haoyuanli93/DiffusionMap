daf5578fc71c0180922c63e74996e9ebd8e9c014
Haoyuan Li <hyli16@stanford.edu>
2018-07-16 11:22:11 -0700

    minor

--- visualization/util.py
+++ visualization/util.py
@@ -1,5 +1,6 @@
 import numpy as np
 import holoviews as hv
+import matplotlib.path as mpltPath
 
 
 def assemble_patterns(data_holder, row_num, col_num, index, pattern_shape):
@@ -34,3 +35,40 @@ def assemble_patterns(data_holder, row_num, col_num, index, pattern_shape):
                              for l in range(index_num, row_num * col_num)})
 
     return hv.GridSpace(image_holder)
+
+
+def save_selected_region(stream_holder, data_holder, output='./selected_index.npy', return_selected_region=False):
+    """
+    Use this function to parse the stream and find the index of the points contained in the specified region.
+    This function also returns the index of the selected
+
+    :param stream_holder: The holoviews stream containing the contour of the path.
+    :param data_holder: The numpy array containing all the positions of each point
+                        [[x, y],
+                         [x, y],
+                         ... ]     This is should be of the shape [number of points, 2]
+
+    :param output: The file name to save the index of points of the selected region
+    :param return_selected_region: Choose whether to return the selected region for inspection
+    :return:
+    """
+    # Extract the x and y coordinates of different points along the path
+    x_coor = stream_holder.data['xs'][0]
+    y_coor = stream_holder.data['ys'][0]
+    path_holder = [(x_coor[l], y_coor[l]) for l in range(len(x_coor))]
+
+    # Construct the matplotlib path object
+    poly_path = mpltPath.Path(path_holder)
+
+    # Use the poly_path object to find the index of the particles that are going to be saved
+    decision = poly_path.contains_points(data_holder)
+
+    # Get the index of the points in the region
+    index = np.arange(decision.shape[0])[decision]
+
+    # Save the selected index
+    np.save(output, index)
+
+    if return_selected_region is True:
+        # return the selected index and the selected points
+        return index, data_holder[index]
