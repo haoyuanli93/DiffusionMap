36f7643abcd5c8171ae082699286f94b3ed064fe
Haoyuan Li <hyli16@stanford.edu>
2018-08-01 21:01:37 -0700

    minor

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ import sys
 # Get the absolute path of this file
 path = sys.path[0]
 
+# Define the holder for the path
+holder = 'This is a path_holder. Please use setup.py to initialize this value.'
+
 # Use fileinput to initialize the path value in scripts.
 for line in fileinput.input(('WeightMat.py', 'EigensSlepc.py', 'Visualization_Example.ipynb'), inplace="True"):
-    line.rstrip().replace('This is a path_holder. Please use setup.py to initialize this value.', path)
+    if holder in line:
+        line.replace(holder, path)
