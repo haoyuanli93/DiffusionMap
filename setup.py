f4c129bc4a5e335714330e6629134f8b6ea8242b
Haoyuan Li <hyli16@stanford.edu>
2018-08-01 20:55:03 -0700

    minor

--- setup.py
+++ setup.py
@@ -6,4 +6,4 @@ path = sys.path[0]
 
 # Use fileinput to initialize the path value in scripts.
 for line in fileinput.input(('WeightMat.py', 'EigensSlepc.py', 'Visualization_Example.ipynb'), inplace="True"):
-    line.replace('This is a path_holder. Please use setup.py to initialize this value.', path)
+    line.rstrip().replace('This is a path_holder. Please use setup.py to initialize this value.', path)
