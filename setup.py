37f0a58cbe94fcbd2e34e630abcf0f0fe8bc5082
Haoyuan Li <hyli16@stanford.edu>
2018-08-08 10:06:24 -0700

    minor

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     version=0.2,
     description=(
         'This package aims to do Diffusion Map in a parallel way. '),
+    long_description=("This package calculate the diffusion map in a parallel manner."
+                      "Please check the github repo at https://github.com/haoyuanli93/DiffusionMap"
+                      "for more detailed description."),
     author='Haoyuan Li',
     author_email='hyli16@stanford.edu',
     maintainer='Haoyuan Li',
