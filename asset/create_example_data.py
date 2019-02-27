5939dd3f9d11a35a0e2b389e146a6d5c56ba23f6
Haoyuan Li <hyli16@stanford.edu>
2019-02-26 20:27:06 -0800

    example

--- asset/create_example_data.py
+++ asset/create_example_data.py
@@ -4,8 +4,8 @@ from scipy import ndimage
 import h5py as h5
 import os
 
-file_num = 5     # Number of h5files to generate
-batch_num = 2    # Number of datasets in each h5file. Each dataset will contain 100 images of shape 256*256
+file_num = 5  # Number of h5files to generate
+batch_num = 2  # Number of datasets in each h5file. Each dataset will contain 100 images of shape 256*256
 print("{} h5 files will be generated. Each will contain {} datasets".format(file_num, batch_num))
 print("There will be 100 images in each dataset. Each dataset is of the shape (100,256,256)")
 
@@ -28,7 +28,7 @@ def shift_to_center(array):
 
     # Shift the pattern
     shifted_array = ndimage.shift(array, shift=[center[l] - center_mass[l] for l in range(dim)])
-    
+
     return shifted_array
 
 
@@ -46,33 +46,46 @@ final_sample = shift_to_center(final_sample_holder)
 # Create the rotated image and save them to the corresponding h5 file
 
 # Generate random angles
-random_angles = np.random.rand(file_num,batch_num,100)*360.
-np.save("../input/random_angles.npy", arr=random_angles)
+random_angles = np.random.rand(file_num * batch_num * 100) * 72
+for l in range(5):
+    random_angles[l * file_num * batch_num:(l + 1) * file_num * batch_num] += 72 * l
+
+# Generate a category array
+category_array = np.zeros_like(random_angles)
+for l in range(5):
+    category_array[l * file_num * batch_num:(l + 1) * file_num * batch_num] = l
+
+np.save("../input/examples_angles.npy", arr=random_angles)
+np.save("../input/examples_category.npy", arr=category_array)
+
 print("Generate {} random angles".format(np.prod(random_angles.shape)))
 print("These random values are saved to the file {}".format("../input/random_angles.npy"))
 
+# Change the shape of the random angle array
+random_angles = random_angles.reshape((file_num, batch_num, 100))
+
 # Loop through all the angles
 for l in range(5):
-    
-    with h5.File('../input/example_data_file_{}.h5'.format(l),'w') as h5file:
-        
+
+    with h5.File('../input/example_data_file_{}.h5'.format(l), 'w') as h5file:
+
         for m in range(2):
-            
+
             # First create a dataset
-            angle_holder = random_angles[l,m]
+            angle_holder = random_angles[l, m]
             dataset_holder = np.zeros((100, 256, 256))
-            
+
             # Loop through all the angles
             for n in range(100):
-                dataset_holder[n]=ndimage.rotate(input=final_sample,
-                                                 angle=angle_holder[n],
-                                                 reshape=False)
-                
-            
-            h5file.create_dataset("Dataset_{}".format(m),data=dataset_holder)
-            
-        print("A new h5 file containing example datasets in created in {}".format('../input/example_data_file_{}.h5'.format(l)))
-        
+                dataset_holder[n] = ndimage.rotate(input=final_sample,
+                                                   angle=angle_holder[n],
+                                                   reshape=False)
+
+            h5file.create_dataset("Dataset_{}".format(m), data=dataset_holder)
+
+        print("A new h5 file containing example datasets in created in {}".format(
+            '../input/example_data_file_{}.h5'.format(l)))
+
 ###############################################
 # Update the input_file_list.txt in the input folder
 ###############################################
@@ -84,7 +97,8 @@ with open('../input/file_list.txt', 'w') as txtfile:
     txtfile.write("# This is a file aims to help you to specify which dataset to process.\n")
     txtfile.write("# This txt file, or any txt file obeying the following roles can\n")
     txtfile.write("# be understood by the program.\n")
-    txtfile.write("# 1. The program ignore any lines that is empty, starts with # or blank space. \n")
+    txtfile.write(
+        "# 1. The program ignore any lines that is empty, starts with # or blank space. \n")
     txtfile.write("# 2. The program can not handle addresses containing any blank spaces.\n")
     txtfile.write("#    Please pay attention to this.\n")
     txtfile.write("# 3. The h5py file address, preferably the absolute address, should \n")
@@ -113,6 +127,5 @@ with open('../input/file_list.txt', 'w') as txtfile:
     txtfile.write("#File:/reg/d/psdm/amo/amo86615/res/haoyuan/multiples.py\n")
     txtfile.write("##################################################################\n")
     for l in range(file_num):
-        txtfile.write("File:{}\n".format(os.path.abspath('../input/example_data_file_{}.h5'.format(l))))
-        
-        
+        txtfile.write(
+            "File:{}\n".format(os.path.abspath('../input/example_data_file_{}.h5'.format(l))))
